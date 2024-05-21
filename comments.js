c12c5c64a48e51788b627a40c0952376ecf4c452
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2024-05-21 18:39:21 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,38 @@
+// Create web server
+// 1. Load the http module
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+
+// 2. Create a server
+http.createServer(function (req, res) {
+    // 3. Parse the request containing file name
+    var pathname = url.parse(req.url).pathname;
+    // 4. Print the name of the file for which request is made.
+    console.log("Request for " + pathname + " received.");
+
+    // 5. Read the requested file content from file system
+    fs.readFile(pathname.substr(1), function (err, data) {
+        if (err) {
+            console.log(err);
+            // HTTP Status: 404 : NOT FOUND
+            // Content Type: text/plain
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+        } else {
+            // Page found	  
+            // HTTP Status: 200 : OK
+            // Content Type: text/plain
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+
+            // Write the content of the file to response body
+            res.write(data.toString());
+        }
+
+        // Send the response body 
+        res.end();
+    });
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://
